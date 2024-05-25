1c70991775ce2f607da3e93184553d48ed391a99
Olesia <lisa999lisa@mail.ru>
2024-05-25 20:50:21 +0300

    initial commit junit4

---
+++ src/test/java/ru/netology/service/CashBackHackServiceTest.java
@@ -0,0 +1,37 @@
+package ru.netology.service;
+import static org.junit.Assert.assertEquals;
+
+public class CashBackHackServiceTest {
+    @org.junit.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+    @org.junit.Test
+    public void testBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        assertEquals(expected, actual);
+    }
+    @org.junit.Test
+    public void testRemainFor1Cashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+    @org.junit.Test
+    public void testRemainFor2Cashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(expected, actual);
+    }
+}
